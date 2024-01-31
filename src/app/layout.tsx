bdc970c5049d02407dba1eb0c5b1a26b8981f8a5
Thanh <thanh@clgt.vn>
2024-01-30 23:59:56 -0700

    Add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
+import Script from 'next/script';
 
 import './globals.css';
 
@@ -26,6 +27,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={roboto.variable}>
       <body className="relative font-sans text-slate-700">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-V63QNXH1EX" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-V63QNXH1EX');
+          `}
+        </Script>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
